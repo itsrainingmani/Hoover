f727c3557f3343909ddd77227bb2e2e4590a0a9d
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-07-16 11:21:03 -0400

    Write the list of shares to a tab separated file

--- hoover/__main__.py
+++ hoover/__main__.py
@@ -1,3 +1,4 @@
+import os
 import pprint
 import requests
 import argparse
@@ -5,7 +6,9 @@ from bs4 import BeautifulSoup
 from hoover import SPLASH_TEXT
 from .utils import mostRecentHolding, getInformationTableUrl, processXml
 
-def main(cik):
+DATA_DIR = 'forms/'
+
+def main(cik, output_file_name):
     try:
         # Try to validate the CIK
         holdings = mostRecentHolding(cik)
@@ -18,19 +21,31 @@ def main(cik):
 
     # Get the URL for Form 13F Information Table
     infotable_url = getInformationTableUrl(document_url)
-    print(infotable_url)
 
     # Download the Form 13F info table, parse the XML and
     # return a list of share and information
     list_of_shares = processXml(infotable_url)
-    pprint.pprint(list_of_shares)
+
+    if not os.path.exists(DATA_DIR):
+        print("Creating directory to hold file outputs\n")
+        os.mkdir(DATA_DIR)
+
+    print("Writing Holding Info to Tab Separated File\n")
+    new_file_name = DATA_DIR + output_file_name + '-%s.tsv' % cik
+    with open(new_file_name, 'w+') as out_file:
+        for sh in list_of_shares:
+            out_file.write('\t'.join(sh) + '\n')
+    print("Done!")
+    return
+
+
 
 if __name__ == '__main__':
     print(SPLASH_TEXT)
 
     parser = argparse.ArgumentParser(description='Extract EDGAR data for a Fund', usage='python -m hoover [-h] CIK [-o | --output file]')
     parser.add_argument('CIK', type=str, help='Central Index Key for the Fund')
-    parser.add_argument('-o', '--output', help='Name of output tsv file', metavar='file')
+    parser.add_argument('-o', '--output', help='Name of output tsv file', metavar='file', default='Form13F')
 
     args = parser.parse_args()
 
@@ -41,4 +56,4 @@ if __name__ == '__main__':
         print("The CIK has to be a valid number")
         exit(-1)
 
-    main(args.CIK)
+    main(args.CIK, output_file_name=args.output)
