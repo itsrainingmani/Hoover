6f24189f451eccef454d11acb7a8b37e81c3baec
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-07-16 11:42:39 -0400

    Add flag to argparse that allows user to check if a timestamp should be included in file name

--- hoover/__main__.py
+++ hoover/__main__.py
@@ -8,7 +8,7 @@ from .utils import mostRecentHolding, getInformationTableUrl, processXml
 
 DATA_DIR = 'forms/'
 
-def main(cik, output_file_name):
+def main(cik, output_file_name, add_timestamp):
     try:
         # Try to validate the CIK
         holdings = mostRecentHolding(cik)
@@ -31,7 +31,15 @@ def main(cik, output_file_name):
         os.mkdir(DATA_DIR)
 
     print("Writing Holding Info to Tab Separated File\n")
-    new_file_name = DATA_DIR + output_file_name + '-%s.tsv' % cik
+    new_file_name = DATA_DIR + output_file_name + '-' + cik
+
+    if add_timestamp:
+        from datetime import datetime
+        time_stamp = datetime.now()
+        new_file_name += '-' + time_stamp.strftime('%Y%m%d_%H-%M-%S')
+    
+    new_file_name += '.tsv'
+
     with open(new_file_name, 'w+') as out_file:
         for sh in list_of_shares:
             out_file.write('\t'.join(sh) + '\n')
@@ -43,10 +51,11 @@ def main(cik, output_file_name):
 if __name__ == '__main__':
     print(SPLASH_TEXT)
 
-    parser = argparse.ArgumentParser(description='Extract EDGAR data for a Fund', usage='python -m hoover [-h] CIK [-o | --output file]')
+    parser = argparse.ArgumentParser(description='Extract EDGAR data for a Fund', usage='python -m hoover [-h] CIK [-o | --output file] [-t | --time]')
     parser.add_argument('CIK', type=str, help='Central Index Key for the Fund')
     parser.add_argument('-o', '--output', help='Name of output tsv file', metavar='file', default='Form13F')
-
+    parser.add_argument('-t', '--time', dest='timestamp', action='store_true', help='Flag to add timestamp to the output file name')
+    parser.set_defaults(timestamp=False)
     args = parser.parse_args()
 
     try:
@@ -56,4 +65,4 @@ if __name__ == '__main__':
         print("The CIK has to be a valid number")
         exit(-1)
 
-    main(args.CIK, output_file_name=args.output)
+    main(args.CIK, args.output, args.timestamp)
