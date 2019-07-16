53ee3568309803d00f8d5982b6caaec2a5e31ee8
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-07-16 10:32:16 -0400

    Call processXML function in main

--- hoover/__main__.py
+++ hoover/__main__.py
@@ -1,22 +1,29 @@
+import pprint
 import requests
 import argparse
 from bs4 import BeautifulSoup
 from hoover import SPLASH_TEXT
-from .utils import mostRecentHolding, getInformationTable
+from .utils import mostRecentHolding, getInformationTableUrl, processXml
 
 def main(cik):
     try:
         # Try to validate the CIK
         holdings = mostRecentHolding(cik)
-        print(holdings)
     except ValueError as err:
         print(err)
         return
 
+    # Get the href value for 'Documents' link in table
     document_url = holdings[1]['href']
-    data = getInformationTable(document_url)
-    print(data)
 
+    # Get the URL for Form 13F Information Table
+    infotable_url = getInformationTableUrl(document_url)
+    print(infotable_url)
+
+    # Download the Form 13F info table, parse the XML and
+    # return a list of share and information
+    list_of_shares = processXml(infotable_url)
+    pprint.pprint(list_of_shares)
 
 if __name__ == '__main__':
     print(SPLASH_TEXT)
