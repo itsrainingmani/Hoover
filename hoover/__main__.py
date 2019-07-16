a26f6e8af1960fc9a5ecc993cf9fd7b5c118c268
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-07-16 17:44:34 -0400

    Display author information under splash text

--- hoover/__main__.py
+++ hoover/__main__.py
@@ -3,8 +3,9 @@ import pprint
 import requests
 import argparse
 from bs4 import BeautifulSoup
-from hoover import SPLASH_TEXT, DATA_DIR
+from hoover import SPLASH_TEXT, DATA_DIR, __author__
 from .utils import mostRecentHolding, getInformationTableUrl, processXml
+
 
 def main(id, output_file_name, add_timestamp):
     try:
@@ -15,7 +16,7 @@ def main(id, output_file_name, add_timestamp):
         return
 
     # Get the href value for 'Documents' link in table
-    document_url = holdings[1]['href']
+    document_url = holdings[1]["href"]
 
     # Get the URL for Form 13F Information Table
     infotable_url = getInformationTableUrl(document_url)
@@ -29,7 +30,7 @@ def main(id, output_file_name, add_timestamp):
         os.mkdir(DATA_DIR)
 
     print("\nWriting Holding Info to Tab Separated File")
-    new_file_name = DATA_DIR + output_file_name + '-' + id
+    new_file_name = DATA_DIR + output_file_name + "-" + id
 
     # If the -t or --time flags were present during invocation
     # Get current timestamp formatted into a string and append
@@ -37,25 +38,42 @@ def main(id, output_file_name, add_timestamp):
     if add_timestamp:
         print("(with timestamp)\n")
         from datetime import datetime
-        time_stamp = datetime.now()
-        new_file_name += '-' + time_stamp.strftime('%Y%m%d_%H-%M-%S')
-    
-    new_file_name += '.tsv'
 
-    with open(new_file_name, 'w+') as out_file:
+        time_stamp = datetime.now()
+        new_file_name += "-" + time_stamp.strftime("%Y%m%d_%H-%M-%S")
+
+    new_file_name += ".tsv"
+
+    with open(new_file_name, "w+") as out_file:
         for sh in list_of_shares:
-            out_file.write('\t'.join(sh) + '\n')
+            out_file.write("\t".join(sh) + "\n")
     print("Done!")
     return
 
-if __name__ == '__main__':
-    print(SPLASH_TEXT)
+
+if __name__ == "__main__":
+    print(SPLASH_TEXT + "By %s\n" % __author__)
 
     # Construct the ArgumentParser
-    parser = argparse.ArgumentParser(description='Extract EDGAR data for a Fund', usage='python -m hoover [-h] ID [-o | --output file] [-t | --time]')
-    parser.add_argument('ID', type=str, help='CIK or Ticker for a Fund')
-    parser.add_argument('-o', '--output', help='Name of output tsv file', metavar='file', default='Form13F')
-    parser.add_argument('-t', '--time', dest='timestamp', action='store_true', help='Flag to add timestamp to the output file name')
+    parser = argparse.ArgumentParser(
+        description="Extract EDGAR data for a Fund",
+        usage="python -m hoover [-h] ID [-o | --output file] [-t | --time]",
+    )
+    parser.add_argument("ID", type=str, help="CIK or Ticker for a Fund")
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="Name of output tsv file",
+        metavar="file",
+        default="Form13F",
+    )
+    parser.add_argument(
+        "-t",
+        "--time",
+        dest="timestamp",
+        action="store_true",
+        help="Flag to add timestamp to the output file name",
+    )
     parser.set_defaults(timestamp=False)
     args = parser.parse_args()
 
